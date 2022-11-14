fdcc42c94ae3cad7e31fc4ca278dffa8c9ef9815
Michael Hong <michaelhong@Michaels-Mac-mini.local>
2022-11-14 10:47:57 -0500

    14.Search Page, 15.Login Required, 16.Postgres Heroku and 17.Linting and Code Formatting -- Complete

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,7 +1,7 @@
-import pytest
-import os
 import json
 from pathlib import Path
+
+import pytest
 
 from project.app import app, db
 
@@ -75,8 +75,13 @@ def test_messages(client):
     assert b"&lt;Hello&gt;" in rv.data
     assert b"<strong>HTML</strong> allowed here" in rv.data
 
+
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
-    rv = client.get('/delete/1')
+    rv = client.get("/delete/1")
+    data = json.loads(rv.data)
+    assert data["status"] == 0
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    rv = client.get("/delete/1")
     data = json.loads(rv.data)
     assert data["status"] == 1
